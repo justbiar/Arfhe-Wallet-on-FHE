88696c0a0ade989632f228a951110dcde30cf4ba
just biar <160464890+justbiar@users.noreply.github.com>
2024-05-08 14:45:21 +0300

    Update index.js

--- Arfhewallet/index.js
+++ Arfhewallet/index.js
@@ -1,6 +1,6 @@
 // En son blok numarasını verecektir
 const Web3 = require('web3');
-const infuraUrl = 'https://sepolia.infura.io/v3/0644d4d0541440a392fa8a067f54afb1D';
+const infuraUrl = '';
 const web3 = new Web3(new Web3.providers.WebsocketProvider(infuraUrl));
 
 web3.eth.getBlockNumber().then(console.log).catch(console.error);
