ddc426297e13a91b500ed6acca31fdd235103684
justbiar <selfbiar@gmail.com>
2025-02-16 18:40:13 +0300

    Eski sürüm v0.00 olarak arşivlendi. Yeni sürüm v0.01 olarak devam edecektir.

--- Arfhewallet/index.js
+++
@@ -1,6 +0,0 @@
-// En son blok numarasını verecektir
-const Web3 = require('web3');
-const infuraUrl = '';
-const web3 = new Web3(new Web3.providers.WebsocketProvider(infuraUrl));
-
-web3.eth.getBlockNumber().then(console.log).catch(console.error);
--- fhEVM/fhevm-library.sol
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: BSD-3-Clause-Clear
-
-pragma solidity ^0.8.20;
-
-import "fhevm/lib/TFHE.sol";
-
