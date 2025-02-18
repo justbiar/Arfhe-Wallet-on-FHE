21a83dce85af2d235fd9934ba42d6d047ad19b12
omer aydogan <fastmers44@gmail.com>
2025-02-18 23:01:41 +0300

    24wordpage

--- src/pages/CreateWith12Word.jsx
+++ src/pages/CreateWith12Word.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import "./CreateWith12Word.css";
 import ImportWalletButton from "../components/Importwalletbutton";
 
+
+
 const CreateWith12Word = () => {
   const words = [
     "aydan", "devin", "kang", "sabak", "crack", "orga",
@@ -11,7 +13,7 @@ const CreateWith12Word = () => {
   return (
     <div className="app-container">
       <div className="card">
-        <h2 className="header">📱 12 Kelime</h2>
+        <h2 className="header"> 12 Kelime</h2>
         <div className="word-container">
           {words.map((word, index) => (
             <div key={index} className="word-box">
