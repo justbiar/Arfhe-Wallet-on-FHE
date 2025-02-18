746c9fa564cd7c8a80f027aae6438ff79b83372a
justbiar <selfbiar@gmail.com>
2025-02-18 14:46:44 +0300

    temel sayfa şablonları oluşturuldu

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
-import { useState } from 'react'
-import './App.css'
+import React from "react";
+import {Routes, Route} from 'react-router-dom'
+import FirstLogin from './pages/FirstLogin';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-    
-    </>
-  )
-}
+    <div>
+      <Routes>
+        <Route path='/' element={<FirstLogin/>} />
+      </Routes>
+    </div>
+  );
+};
 
-export default App
+export default App;
