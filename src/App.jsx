aa0f551bf04c0e8ece6cf2d7f1b68f6e99553d00
justbiar <selfbiar@gmail.com>
2025-02-18 14:49:12 +0300

    Temel sayfa düzeni

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
-import React from "react";
+
+import { useState } from "react";
 import {Routes, Route} from 'react-router-dom'
-import FirstLogin from './pages/FirstLogin';
+import FirstLogin from './pages/FirstLogin'
+import CreateNewWallet from './pages/createNewWallet'
+import Home from './pages/Home'
+import ImportWallet from './pages/ImportWallet'
+
 
 function App() {
   return (
     <div>
       <Routes>
         <Route path='/' element={<FirstLogin/>} />
+        <Route path='createwallet' element={<CreateNewWallet/>} />
+        <Route path='home' element={<Home/>} />
+        <Route path='importwallet' element={<ImportWallet/>} />
+
       </Routes>
     </div>
   );
